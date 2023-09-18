f8eb410d2a809a8cfbb5d86a5bab1820ca8da0a5
Dilip Kumar <curiousdilip@gmail.com>
2023-09-18 22:44:09 +0530

    about page some style

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ProfileCard from '../components/ProfileCard'
+import "./about.css"
 export const metadata = {
     title: "About",
 
@@ -25,22 +26,23 @@ const page = () => {
                             <ProfileCard />
                         </div>
                         <div className="col-md-8">
-                            <h2>Hi  I'm Dilip Kumar
-                            </h2>
-                            <p>I am a frontend web developer with more than 1 year of experience, I'm very passionate about technology and computers. I am reliable, can work in a team, and can learn quickly my interests are in frontend web development. In my free time, I enjoy music, gaming, and reading. As well, I love to travel and see new places and meet new people.
+                            <div className="about-section">
+                                <h2>Hi  I'm Dilip Kumar
+                                </h2>
+                                <p>I am a frontend web developer with more than 1 year of experience, I'm very passionate about technology and computers. I am reliable, can work in a team, and can learn quickly my interests are in frontend web development. In my free time, I enjoy music, gaming, and reading. As well, I love to travel and see new places and meet new people.
 
-                            </p>
-                            <h4>Professional milestones of my career:
-                            </h4>
-                            <h4 className='italic'>TIMELINE</h4>
-                            <p>You can always reach out to me to have a coffee together and discuss Code, Community or Collaboration. Ping me on Telegram, or reach out to me at curiousdilip@gmail.com.
+                                </p>
+                                <h4>Professional milestones of my career:
+                                </h4>
+                                <h4 className='italic'>TIMELINE</h4>
+                                <p className='reach-me'>You can always reach out to me to have a coffee together and discuss Code, Community or Collaboration. Ping me on Telegram, or reach out to me at curiousdilip@gmail.com.
 
-                            </p>
-                            <p>See more about my working experiences at my resume page.
+                                </p>
+                                <p>See more about my working experiences at my resume page.
 
-                            </p>
-                        </div>
-                    </div>
+                                </p>
+                            </div>
+                        </div>                    </div>
                 </div>
             </section>
         </>
