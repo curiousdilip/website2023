53dc1cd7b17816328b70656afccbe1d748c2f672
Dilip Kumar <curiousdilip@gmail.com>
2023-09-22 22:35:26 +0530

    unrelevant package clearing

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,9 +1,44 @@
+"use client"
 import React from 'react'
-export const metadata = {
-    title: "Contact",
-}
+import { useState } from 'react'
+// export const metadata = {
+//     title: "Contact",
+// }
 import "./contact.css"
 const page = () => {
+    const [formSubmitted, setFormSubmitted] = useState(false);
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        try {
+            const response = await fetch(
+                'https://script.google.com/macros/s/AKfycbyLgS0NuKwRbDWDoFgz2FROEOQk6Tc3D-9uxazp4EQct-ENewCyfyRNkmZdB9sdadRZ/exec',
+                {
+                    method: 'POST',
+                    body: new FormData(event.target),
+                }
+            );
+
+            if (response.ok) {
+                // If the response is successful, set the formSubmitted state to true
+                setFormSubmitted(true);
+                // Display an alert box
+                window.alert('Thank You! \n Form submitted successfully!');
+            } else {
+                // Handle errors here if needed
+                console.error('Form submission failed.');
+            }
+        } catch (error) {
+            // Handle network errors here if needed
+            console.error('Network error:', error);
+        }
+    };
+
+    if (formSubmitted) {
+        // If the form has been successfully submitted, reload the page
+        window.location.reload();
+    }
     return (
         <>
             <section id="contact">
@@ -21,7 +56,7 @@ const page = () => {
                     </div>
                     <div className="row align-items-center">
                         <div className="col-md-8">
-                            <form action="https://script.google.com/macros/s/AKfycbyLgS0NuKwRbDWDoFgz2FROEOQk6Tc3D-9uxazp4EQct-ENewCyfyRNkmZdB9sdadRZ/exec" method="POST" >
+                            <form action="https://script.google.com/macros/s/AKfycbyLgS0NuKwRbDWDoFgz2FROEOQk6Tc3D-9uxazp4EQct-ENewCyfyRNkmZdB9sdadRZ/exec" method="POST" onSubmit={handleSubmit} >
                                 <input
                                     type="name"
                                     name="name"
