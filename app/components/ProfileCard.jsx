e6b80293cc13a9be71daa5c6b986411aff0571f9
Dilip Kumar <curiousdilip@gmail.com>
2023-09-27 19:23:48 +0530

    link added to profile card icons

--- app/components/ProfileCard.jsx
+++ app/components/ProfileCard.jsx
@@ -23,14 +23,14 @@ const ProfileCard = () => {
                         <i className='bi bi-briefcase'> Looking for Opportunity</i>
                     </div>
                     <div className="location">
-                        <i className='bi bi-geo'> New Delhi, India</i>
+                        <a href="https://www.google.com/maps/place/Dilip+Kumar+%7C+Frontend+Web+Developer/@28.569776,77.0265519,20.3z/data=!4m6!3m5!1s0x390d1b0024a3520b:0x7cca2b1632be5ac7!8m2!3d28.5695894!4d77.0267466!16s%2Fg%2F11r__7nsmj?entry=ttu" target='_blank'><i className='bi bi-geo'> New Delhi, India</i></a>
                     </div>
                     <div className="mail">
-                        <i className='bi bi-envelope'> curiousdilip@gmail.com</i>
+                        <a href="mailto:curiousdilip@gmail.com"> <i className='bi bi-envelope'> curiousdilip@gmail.com</i></a>
                     </div>
                     <div className="social">
-                        <i className='bi bi-github'> curiousdilip</i>
-                        <i className='bi bi-linkedin'> curiousdilip</i>
+                        <a href="https://github.com/curiousdilip" target='_blank'> <i className='bi bi-github'> curiousdilip</i></a>
+                        <a href="https://www.linkedin.com/in/curiousdilip/" target='_blank'><i className='bi bi-linkedin'> curiousdilip</i></a>
 
                     </div>
                 </div>
