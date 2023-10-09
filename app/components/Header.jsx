61bddfc0f0f1d9f3924ca90026e7b16298d3e65a
Dilip Kumar Maurya <curiousdilip@gmail.com>
2023-10-09 05:05:44 +0000

    hamburger animation

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,108 +1,116 @@
-"use client"
-import React, { useEffect, useState } from 'react';
-import { useRouter } from 'next/navigation';
-import classnames from 'classnames';
-import Image from 'next/image';
-import Link from 'next/link';
-import logoDark from '../../public/logo-black.svg';
-import logoLight from '../../public/logo-white.svg';
-import "./header.css"
+"use client";
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import classnames from "classnames";
+import Image from "next/image";
+import Link from "next/link";
+import logoDark from "../../public/logo-black.svg";
+import logoLight from "../../public/logo-white.svg";
+import "./header.css";
 
 const Header = () => {
-    const router = useRouter();
+  const router = useRouter();
 
-    useEffect(() => {
-        require('bootstrap/dist/js/bootstrap.bundle.min.js');
-    }, []);
+  useEffect(() => {
+    require("bootstrap/dist/js/bootstrap.bundle.min.js");
+  }, []);
 
-    const [scrolled, setScrolled] = useState(false);
-    const [menuOpen, setMenuOpen] = useState(false);
+  const [scrolled, setScrolled] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
-    const handleScroll = () => {
-        setScrolled(window.scrollY > 10);
+  const handleScroll = () => {
+    setScrolled(window.scrollY > 10);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
     };
+  }, []);
 
-    useEffect(() => {
-        window.addEventListener('scroll', handleScroll);
+  const scroll = classnames({
+    scrolled: scrolled,
+  });
 
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        };
-    }, []);
+  const isActive = (pathname) => {
+    return router.pathname === pathname ? "active" : "";
+  };
 
-    const scroll = classnames({
-        scrolled: scrolled,
-    });
+  const logoImage = scrolled ? logoLight : logoDark;
 
-    const isActive = (pathname) => {
-        return router.pathname === pathname ? 'active' : '';
-    };
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
 
-    const logoImage = scrolled ? logoLight : logoDark;
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
 
-    const toggleMenu = () => {
-        setMenuOpen(!menuOpen);
-    };
-
-    const closeMenu = () => {
-        setMenuOpen(false);
-    };
-
-    return (
-        <header className={scroll}>
-            <nav className={`navbar navbar-expand-lg fixed-top ${menuOpen ? 'show' : ''}`}>
-                <div className="container">
-                    <Link href="/" className="navbar-brand" onClick={closeMenu}>
-                        <Image
-                            src={logoImage}
-                            alt="Dilip Logo"
-                            width={45}
-                            height={45}
-                            priority="true"
-                        />
-                        <h1>Dilip Kumar</h1>
-                    </Link>
-                    <button
-                        className="navbar-toggler"
-                        type="button"
-                        data-bs-toggle="collapse"
-                        data-bs-target="#main-header"
-                        aria-controls="main-header"
-                        aria-expanded="false"
-                        aria-label="Toggle navigation"
-                        onClick={toggleMenu} // Toggle mobile menu on button click
-                    >
-                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-list" viewBox="0 0 16 16"><path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"></path></svg>
-                    </button>
-                    <div className={`collapse navbar-collapse ${menuOpen ? 'show' : ''}`} id="main-header">
-                        <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-                            <li className={`nav-item ${isActive('/projects')}`}>
-                                <Link href="/projects" className="nav-link" onClick={closeMenu}>
-                                    Projects
-                                </Link>
-                            </li>
-                            <li className={`nav-item ${isActive('/about')}`}>
-                                <Link href="/about" className="nav-link" onClick={closeMenu}>
-                                    About
-                                </Link>
-                            </li>
-                            <li className={`nav-item ${isActive('/resume')}`}>
-                                <Link href="/resume" className="nav-link" onClick={closeMenu}>
-                                    Resume
-                                </Link>
-                            </li>
-                            <li className={`nav-item ${isActive('/contact')}`}>
-                                <Link href="/contact" className="nav-link" onClick={closeMenu}>
-                                    Contact
-                                </Link>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-        </header>
-    );
+  return (
+    <header className={scroll}>
+      <nav
+        className={`navbar navbar-expand-lg fixed-top ${
+          menuOpen ? "show" : ""
+        }`}
+      >
+        <div className="container">
+          <Link href="/" className="navbar-brand" onClick={closeMenu}>
+            <Image
+              src={logoImage}
+              alt="Dilip Logo"
+              width={45}
+              height={45}
+              priority="true"
+            />
+            <h1>Dilip Kumar</h1>
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#main-header"
+            aria-controls="main-header"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+            onClick={toggleMenu} // Toggle mobile menu on button click
+          >
+            <span class="icon-bar top-bar"></span>
+            <span class="icon-bar middle-bar"></span>
+            <span class="icon-bar bottom-bar"></span>
+          </button>
+          <div
+            className={`collapse navbar-collapse ${menuOpen ? "show" : ""}`}
+            id="main-header"
+          >
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li className={`nav-item ${isActive("/projects")}`}>
+                <Link href="/projects" className="nav-link" onClick={closeMenu}>
+                  Projects
+                </Link>
+              </li>
+              <li className={`nav-item ${isActive("/about")}`}>
+                <Link href="/about" className="nav-link" onClick={closeMenu}>
+                  About
+                </Link>
+              </li>
+              <li className={`nav-item ${isActive("/resume")}`}>
+                <Link href="/resume" className="nav-link" onClick={closeMenu}>
+                  Resume
+                </Link>
+              </li>
+              <li className={`nav-item ${isActive("/contact")}`}>
+                <Link href="/contact" className="nav-link" onClick={closeMenu}>
+                  Contact
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
-
