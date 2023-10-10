0835d72c0f99cb84bb0399525dbfb26f704940bf
Dilip Kumar Maurya <curiousdilip@gmail.com>
2023-10-10 08:58:21 +0000

    ramen paul website added

--- app/data/timeline.js
+++ app/data/timeline.js
@@ -1,23 +1,32 @@
 export const timeline = [
-    {
-        day: "14th April 2022        ",
-        description: "My First Government Project  ",
-        projectName: "Pradhanmantri Sangrahalaya",
-        link: "https://www.pmsangrahalaya.gov.in/"
-    }, {
-        day: "16th February 2022        ",
-        description: "Landed my first job in ",
-        projectName: "Taknik Bharti",
-        link: "https://www.taknikbharti.com/"
-    }, {
-        day: "11th November 2021        ",
-        description: "My First International Project",
-        projectName: "MPAA",
-        link: "https://mpaa.ac.uk/"
-    }, {
-        day: "9th September 2019        ",
-        description: "My First Freelance Project",
-        projectName: "NLP Minerals Pvt. Ltd",
-        link: "https://www.nlpminerals.com/"
-    }
-]
+  {
+    day: "10th September 2023        ",
+    description: "Created 1st React Website for",
+    projectName: "Ramen Paul",
+    link: "https://www.ramenpaul.com/",
+  },
+  {
+    day: "14th April 2022        ",
+    description: "My First Government Project  ",
+    projectName: "Pradhanmantri Sangrahalaya",
+    link: "https://www.pmsangrahalaya.gov.in/",
+  },
+  {
+    day: "16th February 2022        ",
+    description: "Landed my first job in ",
+    projectName: "Taknik Bharti",
+    link: "https://www.taknikbharti.com/",
+  },
+  {
+    day: "11th November 2021        ",
+    description: "My First International Project",
+    projectName: "MPAA",
+    link: "https://mpaa.ac.uk/",
+  },
+  {
+    day: "9th September 2019        ",
+    description: "My First Freelance Project",
+    projectName: "NLP Minerals Pvt. Ltd",
+    link: "https://www.nlpminerals.com/",
+  },
+];
