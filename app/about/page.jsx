d0cc83abbb75e7a1867a5001fbe2fe583020fddc
Dilip Kumar Maurya <curiousdilip@gmail.com>
2023-10-10 09:08:35 +0000

    heading and canonical links fixed

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,52 +1,59 @@
-import React from 'react'
-import ProfileCard from '../components/ProfileCard'
-import "./about.css"
-import TimeLine from '../components/TimeLine'
+import React from "react";
+import ProfileCard from "../components/ProfileCard";
+import "./about.css";
+import TimeLine from "../components/TimeLine";
+import { Helmet } from "react-helmet";
 export const metadata = {
-    title: "About",
+  title: "About",
 };
 const page = () => {
-    return (
-        <>
-            <section id="about">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-md-12">
-                            <div className="heading">
-                                <h1>
-                                    About
-                                </h1>
+  return (
+    <>
+      <Helmet>
+        <title>About | Dilip Kumar</title>
+        <link rel="canonical" href="https://www.dilipmaurya.in/about" />
+      </Helmet>
+      <section id="about">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <div className="heading">
+                <h2>About</h2>
 
-                                <hr />
-                            </div>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-md-4">
-                            <ProfileCard />
-                        </div>
-                        <div className="col-md-8">
-                            <div className="about-section">
-                                <h2>Hi  I'm Dilip Kumar
-                                </h2>
-                                <p>I am a frontend web developer with more than 1 year of experience, I'm very passionate about technology and computers. I am reliable, can work in a team, and can learn quickly my interests are in frontend web development. In my free time, I enjoy music, gaming, and reading. As well, I love to travel and see new places and meet new people.
+                <hr />
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-4">
+              <ProfileCard />
+            </div>
+            <div className="col-md-8">
+              <div className="about-section">
+                <h3>Hi I'm Dilip Kumar</h3>
+                <p>
+                  I am a frontend web developer with more than 1 year of
+                  experience, I'm very passionate about technology and
+                  computers. I am reliable, can work in a team, and can learn
+                  quickly my interests are in frontend web development. In my
+                  free time, I enjoy music, gaming, and reading. As well, I love
+                  to travel and see new places and meet new people.
+                </p>
+                <h4>Professional milestones of my career:</h4>
+                <TimeLine />
+                <p className="reach-me">
+                  You can always reach out to me to have a coffee together and
+                  discuss Code, Community or Collaboration. Ping me on Telegram,
+                  or reach out to me at curiousdilip@gmail.com.
+                </p>
+                <p>See more about my working experiences at my resume page.</p>
+              </div>
+            </div>{" "}
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
 
-                                </p>
-                                <h4>Professional milestones of my career:
-                                </h4>
-                                <TimeLine />
-                                <p className='reach-me'>You can always reach out to me to have a coffee together and discuss Code, Community or Collaboration. Ping me on Telegram, or reach out to me at curiousdilip@gmail.com.
-
-                                </p>
-                                <p>See more about my working experiences at my resume page.
-
-                                </p>
-                            </div>
-                        </div>                    </div>
-                </div>
-            </section>
-        </>
-    )
-}
-
-export default page
+export default page;
--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -4,136 +4,137 @@ import "./contact.css";
 import { Helmet } from "react-helmet";
 
 const Page = () => {
-    const [formSubmitted, setFormSubmitted] = useState(false);
+  const [formSubmitted, setFormSubmitted] = useState(false);
 
-    const handleSubmit = async (event) => {
-        event.preventDefault();
+  const handleSubmit = async (event) => {
+    event.preventDefault();
 
-        try {
-            const response = await fetch(
-                "https://script.google.com/macros/s/AKfycbyLgS0NuKwRbDWDoFgz2FROEOQk6Tc3D-9uxazp4EQct-ENewCyfyRNkmZdB9sdadRZ/exec",
-                {
-                    method: "POST",
-                    body: new FormData(event.target),
-                }
-            );
+    try {
+      const response = await fetch(
+        "https://script.google.com/macros/s/AKfycbyLgS0NuKwRbDWDoFgz2FROEOQk6Tc3D-9uxazp4EQct-ENewCyfyRNkmZdB9sdadRZ/exec",
+        {
+          method: "POST",
+          body: new FormData(event.target),
+        },
+      );
 
-            if (response.ok) {
-                // If the response is successful, set the formSubmitted state to true
-                setFormSubmitted(true);
-                // Display an alert box
-                window.alert("Thank You! \n Form submitted successfully!");
-                // Reset the formSubmitted state after a brief delay (e.g., 2 seconds)
-                setTimeout(() => {
-                    setFormSubmitted(false);
-                }, 2000);
-            } else {
-                // Handle errors here if needed
-                console.error("Form submission failed.");
-            }
-        } catch (error) {
-            // Handle network errors here if needed
-            console.error("Network error:", error);
-        }
-    };
+      if (response.ok) {
+        // If the response is successful, set the formSubmitted state to true
+        setFormSubmitted(true);
+        // Display an alert box
+        window.alert("Thank You! \n Form submitted successfully!");
+        // Reset the formSubmitted state after a brief delay (e.g., 2 seconds)
+        setTimeout(() => {
+          setFormSubmitted(false);
+        }, 2000);
+      } else {
+        // Handle errors here if needed
+        console.error("Form submission failed.");
+      }
+    } catch (error) {
+      // Handle network errors here if needed
+      console.error("Network error:", error);
+    }
+  };
 
-    return (
-        <>
-            <Helmet>
-                <title>Contact | Dilip Kumar</title>
-            </Helmet>
-            <section id="contact">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-md-12">
-                            <div className="heading">
-                                <h1>Contact</h1>
-                                <hr />
-                            </div>
-                        </div>
-                    </div>
-                    <div className="row align-items-center">
-                        <div className="col-md-8">
-                            {!formSubmitted ? (
-                                <form
-                                    action="https://script.google.com/macros/s/AKfycbyLgS0NuKwRbDWDoFgz2FROEOQk6Tc3D-9uxazp4EQct-ENewCyfyRNkmZdB9sdadRZ/exec"
-                                    method="POST"
-                                    onSubmit={handleSubmit}
-                                >
-                                    <input
-                                        type="name"
-                                        name="name"
-                                        className="form-control mb-3"
-                                        placeholder="Name"
-                                        required
-                                    />
-                                    <input
-                                        type="email"
-                                        name="email"
-                                        className="form-control mb-3"
-                                        placeholder="Email"
-                                        required
-                                    />
-                                    <input
-                                        type="tel"
-                                        name="phone"
-                                        className="form-control mb-3"
-                                        placeholder="Phone number"
-                                        required
-                                    />
-                                    <textarea
-                                        name="message"
-                                        className="form-control mb-3"
-                                        rows="3"
-                                        placeholder="Your Message"
-                                    ></textarea>
+  return (
+    <>
+      <Helmet>
+        <title>Contact | Dilip Kumar</title>
+        <link rel="canonical" href="https://www.dilipmaurya.in/contact" />
+      </Helmet>
+      <section id="contact">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <div className="heading">
+                <h2>Contact</h2>
+                <hr />
+              </div>
+            </div>
+          </div>
+          <div className="row align-items-center">
+            <div className="col-md-8">
+              {!formSubmitted ? (
+                <form
+                  action="https://script.google.com/macros/s/AKfycbyLgS0NuKwRbDWDoFgz2FROEOQk6Tc3D-9uxazp4EQct-ENewCyfyRNkmZdB9sdadRZ/exec"
+                  method="POST"
+                  onSubmit={handleSubmit}
+                >
+                  <input
+                    type="name"
+                    name="name"
+                    className="form-control mb-3"
+                    placeholder="Name"
+                    required
+                  />
+                  <input
+                    type="email"
+                    name="email"
+                    className="form-control mb-3"
+                    placeholder="Email"
+                    required
+                  />
+                  <input
+                    type="tel"
+                    name="phone"
+                    className="form-control mb-3"
+                    placeholder="Phone number"
+                    required
+                  />
+                  <textarea
+                    name="message"
+                    className="form-control mb-3"
+                    rows="3"
+                    placeholder="Your Message"
+                  ></textarea>
 
-                                    <button type="submit" className="btn btn-primary">
-                                        Send Now
-                                    </button>
-                                </form>
-                            ) : (
-                                <p>Thank you for submitting the form!</p>
-                            )}
-                        </div>
-                        <div className="col-md-4">
-                            <div className="details">
-                                <div className="box">
-                                    <div className="icon">
-                                        <i className="bi bi-telephone"></i>
-                                    </div>
-                                    <div className="number">
-                                        <h4>Call</h4>
-                                        <p>+91 98997 42615</p>
-                                    </div>
-                                </div>
-
-                                <div className="box">
-                                    <div className="icon">
-                                        <i className="bi bi-envelope"></i>
-                                    </div>
-                                    <div className="mail">
-                                        <h4>Send Email</h4>
-                                        <p>curiousdilip@gmail.com</p>
-                                    </div>
-                                </div>
-
-                                <div className="box">
-                                    <div className="icon">
-                                        <i className="bi bi-geo"></i>
-                                    </div>
-                                    <div className="location">
-                                        <h4>Visit </h4>
-                                        <p>Dwarka, New Delhi, India</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                  <button type="submit" className="btn btn-primary">
+                    Send Now
+                  </button>
+                </form>
+              ) : (
+                <p>Thank you for submitting the form!</p>
+              )}
+            </div>
+            <div className="col-md-4">
+              <div className="details">
+                <div className="box">
+                  <div className="icon">
+                    <i className="bi bi-telephone"></i>
+                  </div>
+                  <div className="number">
+                    <h4>Call</h4>
+                    <p>+91 98997 42615</p>
+                  </div>
                 </div>
-            </section>
-        </>
-    );
+
+                <div className="box">
+                  <div className="icon">
+                    <i className="bi bi-envelope"></i>
+                  </div>
+                  <div className="mail">
+                    <h4>Send Email</h4>
+                    <p>curiousdilip@gmail.com</p>
+                  </div>
+                </div>
+
+                <div className="box">
+                  <div className="icon">
+                    <i className="bi bi-geo"></i>
+                  </div>
+                  <div className="location">
+                    <h4>Visit </h4>
+                    <p>Dwarka, New Delhi, India</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Page;
